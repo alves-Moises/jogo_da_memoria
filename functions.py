8bcc93b4cb514d230a53fea92e6d4020fe180ded
alves-Moises <moisesingo_ra@hotmail.com>
2021-06-06 23:12:06 -0300

    update: layout

--- functions.py
+++ functions.py
@@ -10,32 +10,13 @@ game = True
 # ========functions ========
 def imprimir_quadro(line_1, line_2, line_3, line_4): 
     print('='  * 20)
-    
+    lista_linhas = [line_1, line_2, line_3, line_4]
+    for lista in lista_linhas:
+        print('')
+        i = 0
+        print('| ', end = '')
+        while i < 4:
+            print(lista[i], '', end = '')
+            i += 1
+        print('|', end = '')
     print('')
-    i = 0
-    print('| ', end = '')
-    while i < 4:
-        print(line_1[i], '', end = '')
-        i += 1
-    print('|')
-
-    i = 0
-    print('| ', end = '')
-    while i < 4:
-        print(line_2[i], '', end = '')
-        i += 1
-    print('|')
-
-    i = 0
-    print('| ', end = '')
-    while i < 4:
-        print(line_3[i], '', end = '')
-        i += 1
-    print('|')
-
-    i = 0
-    print('| ', end = '')
-    while i < 4:
-        print(line_4[i], '', end = '')
-        i += 1
-    print('|')
