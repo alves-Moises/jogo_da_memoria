b9be2d761363a2c08dbd7a003fc7c9078161bdc2
alves-Moises <moisesingo_ra@hotmail.com>
2021-06-07 00:33:28 -0300

    update: coment

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ def main():
     lista_resultado = random.sample(functions.lista_itens, 16)
     lista_imprimir = ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x']
     print('x' * 10, 'Jodo da memória', 'x' * 10)
+
+    #======= jogo looping ========
     while game == True:
         functions.imprimir_quadro(functions.linha_1, functions.linha_2, functions.linha_3, functions.linha_4)
         
